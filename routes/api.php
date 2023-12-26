ad141e1c4819551323cffac9ea0af2373aba9db9
gersar08 <nagerman26@gmail.com>
2023-12-26 13:27:10 -0600

    Update search route in api.php

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/registro/daily', [RegistroVentasDailyController::class, 'store']);
     Route::put('/registro/daily/{id}', [RegistroVentasDailyController::class, 'update']);
     Route::delete('/registro/daily/{id}', [RegistroVentasDailyController::class, 'destroy']);
-    Route::get('/registro/daily/search/{field}/{query}', [RegistroVentasDailyController::class, 'search']);
+    Route::get('/registro/daily/search/{query}', [RegistroVentasDailyController::class, 'search']);
     Route::get('/registro/daily/view', [RegistroVentasDailyController::class, 'index']);
 
     Route::post('/registro/weekly', [RegistroVentasWeeklyController::class, 'store']);
