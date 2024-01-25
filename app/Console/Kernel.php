148a9cb7492ed51f3bc8edfb24bf4588ff657f53
gersar08 <nagerman26@gmail.com>
2024-01-25 12:01:34 -0600

    Update daily schedule time to 12:03

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('delete:registroventasdaily')->dailyAt('01:00');
+        $schedule->command('delete:registroventasdaily')->dailyAt('12:03');
     }
 
     /**
