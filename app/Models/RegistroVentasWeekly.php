13764857c75a59ca70f9665b9682ec49688a3235
gersar08 <nagerman26@gmail.com>
2024-02-01 02:27:12 -0600

    Update column names in RegistroVentasWeekly model

--- app/Models/RegistroVentasWeekly.php
+++ app/Models/RegistroVentasWeekly.php
@@ -4,13 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Cliente;
 
 
 class RegistroVentasWeekly extends Model
 {
     use HasFactory;
     protected $table = 'registro_ventas_weekly';
-    protected $fillable = ['cliente_id', 'fardos', 'garrafas', 'pet'];
+    protected $fillable = ['cliente_id', 'fardo', 'garrafa', 'pet'];
 
 }
